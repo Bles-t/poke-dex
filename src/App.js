ac23960e1d406f14383920accba0c7de10656cef
Adrian Bryant <127413341+Bles-t@users.noreply.github.com>
2026-01-30 21:15:19 -0600

    fix routing: auto-redirect / to StartPage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Router, Routes, Route, useNavigate } from 'react-router-dom';
+import { Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import StartPage from './StartPage';
 import PokemonPage from './PokemonPage';
 import Register from './views/register';
@@ -20,12 +20,15 @@ function App() {
 
   return (
     <div>
+
       <Routes>
+        <Route path="/" element={<Navigate to="/StartPage" replace />} />
         <Route path="/StartPage" element={<StartPage />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} /> {/* Use the Login component */}
         <Route path="/PokemonPage" element={<PokemonPage />} />
       </Routes>
+
     </div>
 
   );
