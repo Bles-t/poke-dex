c00852039565f53168fb5f0fe08147f0ddfe48db
Adrian Bryant <127413341+Bles-t@users.noreply.github.com>
2026-02-01 20:19:19 -0600

    removed more login login logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import StartPage from './StartPage';
 import PokemonPage from './PokemonPage';
-import Register from './views/register';
-import Login from './views/login';
+
 
 function App() {
   return (
@@ -11,8 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/StartPage" replace />} />
         <Route path="/StartPage" element={<StartPage />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} /> {/* Use the Login component */}
         <Route path="/PokemonPage" element={<PokemonPage />} />
       </Routes>
 
